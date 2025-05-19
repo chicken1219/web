29b298e0fcf314bd813332fcc6add4be17480fd1
chicken1219 <masterchicken532@gmail.com>
2025-05-19 17:01:25 -0600

    Update script.js

--- pagina/js/script.js
+++ pagina/js/script.js
@@ -1,13 +1,28 @@
 function togglePlatoInfo(button) {
     const platoDiv = button.parentNode;
-    platoDiv.classList.toggle("expanded")
+    platoDiv.classList.toggle("expanded");
     button.textContent = platoDiv.classList.contains("expanded") ? "Ver menos" : "Ver mas";
     const comboInfo = platoDiv.querySelector(".combo-info");
     if (comboInfo) {
-        comboInfo.style.display= platoDiv.classList.contains("expanded") ? "block" : "none";
+        comboInfo.style.display = platoDiv.classList.contains("expanded") ? "block" : "none";
     }
     const platoImagen = platoDiv.querySelector(".plato-imagen");
     if (platoImagen) {
-        platoImagen.style.display= platoDiv.classList.contains("expanded") ? "block" : "none";
+        platoImagen.style.display = platoDiv.classList.contains("expanded") ? "block" : "none";
     }
 }
+
+// Obtén todos los botones "Ver más"
+const verMasButtons = document.querySelectorAll('.ver-mas-btn');
+
+// Agrega event listeners para 'click' y 'touchstart' a cada botón
+verMasButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        togglePlatoInfo(this);
+    });
+    button.addEventListener('touchstart', function(event) {
+        // Evita el comportamiento predeterminado del 'touchstart' para que no interfiera con el 'click'
+        event.preventDefault();
+        togglePlatoInfo(this);
+    });
+});
